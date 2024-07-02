3b8500017b3933088d4e4c11663134bd188ccf4a
TharushiSamindara <tharushisamindara@gmail.com>
2024-07-02 14:43:24 +0530

    Mood tracking system

--- BackEnd/moodTrack/src/main/java/com/example/moodTrack/MoodTrackApplication.java
+++ BackEnd/moodTrack/src/main/java/com/example/moodTrack/MoodTrackApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MoodTrackApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MoodTrackApplication.class, args);
 	}
 
